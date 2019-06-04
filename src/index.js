75eabb4b9107f155a49529442d5b2a5e0c2e1e37
Bruno De Masi <brunodemasi1@gmail.com>
2019-06-04 00:35:55 -0300

    Fixed maybe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const server = require('./server')
 
 server.listen(process.env.PORT || 8888)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 /* eslint-disable class-methods-use-this */
-require('dotenv').config()
-
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
